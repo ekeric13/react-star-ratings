7b3a388616a27986e4fe7463088aa1c47f0cffec
Eric Kennedy <ekeric13@gmail.com>
2017-04-04 21:41:03 -0700

    Add in example and esModule build, along with quick test setup

---
+++ Makefile
@@ -0,0 +1,2 @@
+SERVER_FLAGS = -p 5000 ./example
+include node_modules/react-fatigue-dev/Makefile
---
+++ build/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _starRatings = require('./star-ratings');
+
+var _starRatings2 = _interopRequireDefault(_starRatings);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _starRatings2.default;
