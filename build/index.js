628afc0b5af26afb2490e772d51115ae58486209
ekeric13 <ekeric13@gmail.com>
2018-02-06 14:02:48 -0800

    Add polyfill for Number.isInteger

--- build/index.js
+++ build/index.js
@@ -10,4 +10,9 @@ var _starRatings2 = _interopRequireDefault(_starRatings);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// polyfill for ie
+Number.isInteger = Number.isInteger || function (value) {
+  return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
+};
+
 exports.default = _starRatings2.default;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// polyfill for ie
+Number.isInteger = Number.isInteger || function(value) {
+  return typeof value === 'number' && 
+    isFinite(value) && 
+    Math.floor(value) === value;
+};
+
+
 import StarRatings from './star-ratings';
 
 export default StarRatings;
